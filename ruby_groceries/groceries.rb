5f9f0ee5871056925715c0cd70a6374d5575fa1f
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-02-14 15:44:12 -0500

    removed solution

--- ruby_groceries/groceries.rb
+++ ruby_groceries/groceries.rb
@@ -1,12 +1,1 @@
 groceries = []
-
-while true
-  puts "#{groceries.length} so far. Enter another grocery item, or type 'done':"
-  grocery = gets.chomp
-
-  break if grocery == "done"
-  groceries.push(grocery)
-end
-
-puts "You have #{groceries.length} groceries on your list."
-puts "The groceries are: #{groceries.join(', ')}"
